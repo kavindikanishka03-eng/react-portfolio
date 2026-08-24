a3dec3ecb52ac176027b3f43d4cc68de5a80b1b4
vidurangi99 <vidumalsada1999@gmail.com>
2026-08-24 21:32:16 +0530

    Add base path config

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import { defineConfig } from 'vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/react-portfolio/', // අන්න මේ ලයින් එක අනිවාර්යයෙන්ම දාන්න!
 })
